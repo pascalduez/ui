1ea21c546de670f5b8f36ce486fcd9dcc47f45e4
Pascal Duez <public@pascalduez.me>
2020-11-23 12:16:13 +0100

    Init prefers reduced motion hook

--- packages/hooks/index.js
+++ packages/hooks/index.js
@@ -2,3 +2,5 @@ export { useEnvironment } from './useEnvironment';
 export { useEventCallback } from './useEventCallback';
 export { useLayoutEffect } from './useLayoutEffect';
 export { useInteractAway } from './useInteractAway';
+export { useAnimation } from './useAnimation';
+export { usePrefersReducedMotion } from './usePrefersReducedMotion';
---
+++ packages/hooks/src/usePrefersReducedMotion.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { canUseDOM } from '@ui/dom';
+import { useLayoutEffect } from './useLayoutEffect';
+
+/**
+ * We should probably use addEventListener('change'),
+ * but this is not available prior to Safari 14, so we
+ * stick with AddListener, removeListener for now.
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList
+ */
+
+const QUERY = '(prefers-reduced-motion: no-preference)';
+
+const getInitialState = () => {
+  return canUseDOM ? !window.matchMedia(QUERY).matches : true;
+};
+
+export function usePrefersReducedMotion() {
+  const [prefersReducedMotion, setPrefersReducedMotion] = useState(
+    getInitialState
+  );
+
+  useLayoutEffect(() => {
+    const mediaQueryList = window.matchMedia(QUERY);
+    const listener = evt => {
+      setPrefersReducedMotion(!evt.matches);
+    };
+    mediaQueryList.addListener(listener);
+
+    return () => {
+      mediaQueryList.removeListener(listener);
+    };
+  }, []);
+
+  return prefersReducedMotion;
+}
